3bfd014eb973fea7cb1e7b95950b4cbc3cd32bd3
Charlie Carroll <55137243+Chaz0412@users.noreply.github.com>
2021-01-14 20:08:46 +0000

    Version 1.0

--- Public/JS/ShooterPractice.js
+++ Public/JS/ShooterPractice.js
@@ -15,14 +15,18 @@ var config = {
 };
 
 var bullets;
+var bigBullets;
+var bigSword = 3;
 var ship;
 var enemy;
+var wall;
 var speed;
-var enemySpeed;
+var enemySpeed = 10;
 var stats;
 var cursors;
 var score = 0;
 var scoreText;
+var bigSwordText;
 var gameOver = false;
 var currentSword = 'bullet'
 var lastFired = 0;
@@ -36,6 +40,7 @@ function preload ()
     this.load.image('bullet', 'assets/GameAssets/Swords/shot-sword.png');
     this.load.image('background', 'assets/Level.png');
     this.load.image('enemy', 'assets/GameAssets/Enemy/enemy.png');
+    this.load.image('wall', 'assets/GameAssets/Walls/walltilesheet.png');
 }
 
 function create ()
@@ -50,7 +55,7 @@ function create ()
             function Bullet (scene)
             {
                 Phaser.GameObjects.Image.call(this, scene, 0, 0, currentSword);
-                this.speed = Phaser.Math.GetSpeed(550, 1);
+                this.speed = Phaser.Math.GetSpeed(350, 1);
             },
 
         fire: function (x, y)
@@ -61,10 +66,42 @@ function create ()
             this.setVisible(true);
         },
 
-        updateSword: function()
+        update: function (time, delta)
         {
-            Phaser.GameObjects.Image.call(this, scene, 0, 0, currentSword);
+            this.y -= this.speed * delta;
+
+            if (this.y < -50)
+            {
+                this.setActive(false);
+                this.setVisible(false);
+            }
+        }
+
+    });
+
+    var BigBullet = new Phaser.Class({
+
+        Extends: Phaser.GameObjects.Image,
+
+        initialize:
+
+            function BigBullet (scene)
+            {
+                Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet2');
+                this.speed = Phaser.Math.GetSpeed(350, 1);
+            },
+
+        fire: function (x, y)
+        {
+            this.setPosition(x, y - 50);
+
+            this.setActive(true);
+            this.setVisible(true);
+
+            bigSword = bigSword - 1;
+            bigSwordText.setText('Big Sword Uses:' + bigSword);
         },
+
 
         update: function (time, delta)
         {
@@ -84,6 +121,12 @@ function create ()
 
     bullets = this.physics.add.group({
         classType: Bullet,
+        maxSize: 3,
+        runChildUpdate: true
+    });
+
+    bigBullets = this.physics.add.group({
+        classType: BigBullet,
         maxSize: 1,
         runChildUpdate: true
     });
@@ -93,35 +136,47 @@ function create ()
 
     this.add.image(400, 300, 'background')
 
+    wall = this.physics.add.image(400, 710, 'wall').setScale(17);
+
     ship = this.physics.add.sprite(400, 550, 'ship').setDepth(1);
 
-    enemy = this.physics.add.sprite(400, 100, 'enemy');
+    enemy = this.physics.add.group({
+        key: 'enemy',
+        repeat: 4,
+        setXY: { x: 200, y: 100, stepX: 100}
+    });
 
-    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '72px', fill: '#FFFFFF'});
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFFFFF'});
+
+    bigSwordText = this.add.text(316, 16, 'Big Sword Uses: 0', { fontSize: '32px', fill: '#FFFFFF'});
+
+    bigSwordText.setText('Big Sword Uses:' + bigSword);
 
     cursors = this.input.keyboard.createCursorKeys();
 
     speed = Phaser.Math.GetSpeed(300, 1);
 
-    enemySpeed = Phaser.Math.GetSpeed(30,1)
-
     this.physics.add.collider(bullets, enemy, enemyHit, null, this);
 
+    this.physics.add.collider(bigBullets, enemy, enemyHit, null, this);
+
     this.physics.add.collider(ship, enemy, playerHit, null, this);
+    this.physics.add.collider(wall, enemy, playerHit, null, this);
 
     ship.setCollideWorldBounds(true);
 }
 
 function update (time, delta)
 {
-    var bullet = bullets.get();
+
 
     if (gameOver)
     {
         return;
     }
 
-    enemy.y += enemySpeed * delta;
+    //enemy.y += enemySpeed;
+    enemy.setVelocity(Phaser.Math.Between(1, 1), enemySpeed);
 
     if (cursors.left.isDown)
     {
@@ -134,7 +189,7 @@ function update (time, delta)
 
     if (cursors.up.isDown && time > lastFired)
     {
-
+        var bullet = bullets.get();
 
         if (bullet)
         {
@@ -144,26 +199,46 @@ function update (time, delta)
         }
     }
 
-    if(score >= 10)
+    if(bigSword >= 1)
     {
-        currentSword == 'bullet2';
+        if (cursors.down.isDown && time > lastFired)
+        {
+
+            var bigBullet = bigBullets.get();
+
+            if (bigBullet)
+            {
+                bigBullet.fire(ship.x, ship.y);
+
+                lastFired = time + 50;
+            }
+
+        }
 
     }
 
-
 }
 
-function changeSword()
+function enemyHit(ship, enemys)
 {
-
-}
-
-function enemyHit()
-{
-    enemy.disableBody(true, true);
+    enemys.disableBody(true, true);
     score += 10;
     scoreText.setText('Score: ' + score);
-    bullets.setVisible(false);
+    //bullets.setVisible(false);
+    //bullets.disableBody(true, true);
+    enemySpeed = enemySpeed + 1;
+
+    if (enemy.countActive(true) === 0)
+    {
+        enemy.children.iterate(function (child){
+        child.enableBody(true, child.x, 0, true, true);
+        });
+        bigSword = bigSword + 1;
+        bigSwordText.setText('Big Sword Uses:' + bigSword);
+    }
+
+
+
 
 }
 
@@ -172,6 +247,8 @@ function playerHit()
     this.physics.pause();
 
     ship.setTint(0xff0000);
+
+    var gameOverText = this.add.text(200, 200, 'GAME OVER', { fontSize: '72px', fill: '#FFFFFF'});
 
     gameOver = true;
 }
