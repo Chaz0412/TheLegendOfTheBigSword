6d65aa1719eb19570d5d84e3aa39f6051cb10d63
Charlie Carroll <55137243+Chaz0412@users.noreply.github.com>
2021-01-14 03:08:52 +0000

    player can be killed enemies can be killed, its a dog eat dog world

--- Public/JS/ShooterPractice.js
+++ Public/JS/ShooterPractice.js
@@ -1,9 +1,12 @@
 var config = {
-    type: Phaser.WEBGL,
+    type: Phaser.AUTO,
     width: 800,
     height: 600,
     backgroundColor: '#2d2d2d',
     parent: 'game',
+    physics:{
+        default: 'arcade'
+    },
     scene: {
         preload: preload,
         create: create,
@@ -13,22 +16,31 @@ var config = {
 
 var bullets;
 var ship;
+var enemy;
 var speed;
+var enemySpeed;
 var stats;
 var cursors;
+var score = 0;
+var scoreText;
+var gameOver = false;
+var currentSword = 'bullet'
 var lastFired = 0;
 
 var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('ship', 'assets/sprites/ship.png');
-    this.load.image('bullet', 'assets/sprites/bullet.png');
-    this.load.image('background', 'assets/GameAssets/Background/WebDevBackground.png')
+    this.load.image('ship', 'assets/Player/BlueKnight_entity_000_basic attack 1_000.png');
+    this.load.image('bullet2', 'assets/long-sword.png');
+    this.load.image('bullet', 'assets/GameAssets/Swords/shot-sword.png');
+    this.load.image('background', 'assets/Level.png');
+    this.load.image('enemy', 'assets/GameAssets/Enemy/enemy.png');
 }
 
 function create ()
 {
+
     var Bullet = new Phaser.Class({
 
         Extends: Phaser.GameObjects.Image,
@@ -37,24 +49,28 @@ function create ()
 
             function Bullet (scene)
             {
-                Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
-
-                this.speed = Phaser.Math.GetSpeed(400, 1);
+                Phaser.GameObjects.Image.call(this, scene, 0, 0, currentSword);
+                this.speed = Phaser.Math.GetSpeed(550, 1);
             },
 
         fire: function (x, y)
         {
-            this.setPosition(x + 50 , y - 0);
+            this.setPosition(x, y - 50);
 
             this.setActive(true);
             this.setVisible(true);
         },
 
+        updateSword: function()
+        {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, currentSword);
+        },
+
         update: function (time, delta)
         {
-            this.x += this.speed * delta;
+            this.y -= this.speed * delta;
 
-            if (this.x <  50)
+            if (this.y < -50)
             {
                 this.setActive(false);
                 this.setVisible(false);
@@ -63,39 +79,62 @@ function create ()
 
     });
 
-    bullets = this.add.group({
+    //  Limited to 20 objects in the pool, not allowed to grow beyond it
+    // bullets = this.pool.createObjectPool(Bullet, 20);
+
+    bullets = this.physics.add.group({
         classType: Bullet,
-        maxSize: 1000,
+        maxSize: 1,
         runChildUpdate: true
     });
 
-    this.add.image(400, 300, 'background');
+    //  Create the objects in advance, so they're ready and waiting in the pool
+    bullets.createMultiple({ quantity: 10, active: false });
 
-    ship = this.add.sprite(50, 300, 'ship').setDepth(1);
+    this.add.image(400, 300, 'background')
+
+    ship = this.physics.add.sprite(400, 550, 'ship').setDepth(1);
+
+    enemy = this.physics.add.sprite(400, 100, 'enemy');
+
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '72px', fill: '#FFFFFF'});
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
     speed = Phaser.Math.GetSpeed(300, 1);
 
-    //this.physics.add.collider(player, platforms);
+    enemySpeed = Phaser.Math.GetSpeed(30,1)
+
+    this.physics.add.collider(bullets, enemy, enemyHit, null, this);
+
+    this.physics.add.collider(ship, enemy, playerHit, null, this);
+
+    ship.setCollideWorldBounds(true);
 }
 
 function update (time, delta)
 {
-    if (cursors.up.isDown)
+    var bullet = bullets.get();
+
+    if (gameOver)
     {
-        ship.y -= speed * delta;
-    }
-    else if (cursors.down.isDown)
-    {
-        ship.y += speed * delta;
+        return;
     }
 
-    if (spacebar.isDown && time > lastFired)
+    enemy.y += enemySpeed * delta;
+
+    if (cursors.left.isDown)
     {
-        var bullet = bullets.get();
+        ship.x -= speed * delta;
+    }
+    else if (cursors.right.isDown)
+    {
+        ship.x += speed * delta;
+    }
+
+    if (cursors.up.isDown && time > lastFired)
+    {
+
 
         if (bullet)
         {
@@ -104,4 +143,35 @@ function update (time, delta)
             lastFired = time + 50;
         }
     }
+
+    if(score >= 10)
+    {
+        currentSword == 'bullet2';
+
+    }
+
+
+}
+
+function changeSword()
+{
+
+}
+
+function enemyHit()
+{
+    enemy.disableBody(true, true);
+    score += 10;
+    scoreText.setText('Score: ' + score);
+    bullets.setVisible(false);
+
+}
+
+function playerHit()
+{
+    this.physics.pause();
+
+    ship.setTint(0xff0000);
+
+    gameOver = true;
 }
